784ef294e6026393a0fdcd37f86263fd8b97ebc6
Yeisi\YeisiPC <jose.alcala@potros.itson.edu.mx>
2023-11-26 20:51:28 -0600

    Creacion de repositorio e inicio del proycto.

---
+++ proyectoHotolesMongoDB/src/main/java/org/itson/proyectohotolesmongodb/ProyectoHotolesMongoDB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.proyectohotolesmongodb;
+
+/**
+ *
+ * @author José Carlos Alcalá Ruíz 180872
+ */
+public class ProyectoHotolesMongoDB {
+
+    public static void main(String[] args) {
+        
+    }
+}
